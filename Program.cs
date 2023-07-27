3383e4925ce612cd4bc306544962c23bad968f97
Mirsaid04 <mirsaidd04@gmail.com>
2023-07-27 15:21:04 +0900

    New-C#-FInal-Project

--- Program.cs
+++ Program.cs
@@ -4,42 +4,48 @@
     { 
         static void Main() 
         { 
-                Console.WriteLine("\nHello, Welcome to my project which can calculate the circle of Area and also Perimeter too");
-                Console.WriteLine("Do you want to know the circle of Tringle or primeter of Rectangle?");
-                Console.WriteLine("1.Triangle\n2.Rectangle");
+            string yesOrno=(""); 
+            do{
+            Console.WriteLine("\nHello, Welcome to my project which can calculate the circle of Area and also Perimeter too");
+            Console.WriteLine("Do you want to know the circle of Tringle or primeter of Rectangle?");
+            Console.WriteLine("1.Triangle\n2.Rectangle");
 
             int choosedNumber=int.Parse(Console.ReadLine()!);
-
-            if(choosedNumber==1)
+            switch(choosedNumber)
             {
-                Console.Write("Please, enter the radius:");
-                double radius=Convert.ToDouble(Console.ReadLine()); 
+                case 1 : 
+                    Console.Write("Please, enter the radius:");
+                    double radius=Convert.ToDouble(Console.ReadLine()); 
 
-                Console.WriteLine("\nIt is calculating now ...\n"); 
-                double Result=Math.PI*radius*radius; 
+                    Console.WriteLine("\nIt is calculating now ...\n"); 
+                    double Result=Math.PI*radius*radius; 
  
-                Console.WriteLine($"The circle of Triangle is {Result}\n"); 
-                Console.WriteLine("/\\ <---Thank you---> /\\\n");
-            }
-            else if(choosedNumber==2)
-            {
-                Console.Write("Please enter the breadth: ");
-                decimal breadth=Convert.ToDecimal(Console.ReadLine()!);
+                    Console.WriteLine($"The circle of Triangle is {Result}\n"); 
+                    Console.WriteLine("---Thank you---\n");
+                    break;
+            
+                    case 2:
+                    Console.Write("Please enter the height: ");
+                    decimal height=Convert.ToDecimal(Console.ReadLine()!);
 
-                Console.Write("Please enter the lenght: ");
-                decimal lenght=Convert.ToDecimal(Console.ReadLine());
+                    Console.Write("Please enter the lenght: ");
+                    decimal lenght=Convert.ToDecimal(Console.ReadLine());
 
-                decimal result=(breadth+lenght)*2;
-                Console.WriteLine("\nIt is calculating now ...\n");
+                    decimal result=(height+lenght)*2;
+                    Console.WriteLine("\nIt is calculating now ...\n");
 
-                Console.WriteLine($"The perimeter of Rectangle is {result}cm\n");
-                Console.WriteLine("/\\ <---Thank you---> /\\\n");
+                    Console.WriteLine($"The perimeter of Rectangle is {result}cm\n");
+                    Console.WriteLine("---Thank you---\n");
+                    break;
+
+                default :
+                    Console.WriteLine("Sorry,You entered invalid input, try again later");
+                    Console.WriteLine("\n---Thank you---\n");
+                    break;
             }
-            else 
-            {
-                System.Console.WriteLine("Sorry,You entered invalid input, try again later");
-                Console.WriteLine("\n/\\ <---Thank you---> /\\\n");
-            }
+            Console.Write("Do you want to continue [y/n]: "); 
+            yesOrno=Console.ReadLine()!; 
+            }while(yesOrno=="y");
         } 
     } 
 }
